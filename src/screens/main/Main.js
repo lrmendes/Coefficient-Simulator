673ecce2ad19e18797daa6855d382846b61b919d
lrmendes <lrmen14@gmail.com>
2020-06-07 22:58:00 +0100

    Main Screen - Design Interface Complete

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {useState} from 'react';
-import { View,Text, StyleSheet,ImageBackground,TouchableOpacity, FlatList, SafeAreaView  } from 'react-native';
+import { View,Text, StyleSheet,ImageBackground,TouchableOpacity, FlatList, SafeAreaView, StatusBar  } from 'react-native';
 import { AsyncStorage } from 'react-native';
 import { useEffect } from 'react';
 import bgFile from '../../assets/background2.png';
@@ -50,6 +50,7 @@ export default function HomeScreen(props) {
 
   return (
       <ImageBackground source={bgFile} style={styles.bgimage}>
+        <StatusBar barStyle="light-content" backgroundColor="#002033" />
         <View style={styles.container}>
           <View style={styles.containerItemTop}>
             <Text style={styles.textTop}>Coeficiente Base: 80.4</Text>
@@ -68,10 +69,14 @@ export default function HomeScreen(props) {
               renderItem={({ item }) => (
                 <View key={item} style={styles.listContainer}>
                   <View key={item} style={styles.listItem}>
-                    <View></View>
                     <View>
-                      <Text>Disciplina {item}</Text>
-                      <Text>Carga Horaria: {item*10}h</Text>
+                      <View style={styles.boxDiscCoef}>
+                        <Text style={styles.discCoef}>{10-item}.0</Text>
+                      </View>
+                    </View>
+                    <View style={styles.boxDiscMain}>
+                      <Text style={styles.textDiscName}>Disciplina {item}</Text>
+                      <Text style={styles.textDiscCh}>Carga Horaria: {item*10}h</Text>
                     </View>
                     <View>
                       <TouchableOpacity style={styles.actionList} onPress={() => doNothing(id)}>
@@ -118,18 +123,40 @@ containerItemTop: {
 },
 scrollView: {
   marginBottom: 15,
-  marginTop: 5,
   width: '100%',
 },
 listItem: {
-  padding: 15,
+  paddingLeft: 10,
+  paddingRight: 5,
+  paddingTop: 10,
+  paddingBottom: 10,
   flexDirection: 'row',
   width: '95%',
   alignItems: 'center',
-  justifyContent: 'space-between',
+},
+boxDiscMain: {
+  flex: 1,
+  flexBasis: 0,
+  flexGrow: 1,
+  marginLeft: 10,
+  marginRight: 10,
+  //alignItems: 'center',
 },
 listContainer: {
   width: '100%',
+},
+boxDiscCoef: {
+  backgroundColor: '#004772',
+  borderRadius: 15, 
+  width: 50,
+  height: 50,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+discCoef: {
+  fontSize: 22,
+  marginBottom: 2,
+  color: '#FFFFFF',
 },
 actionList: {
   borderWidth: 1,
@@ -150,12 +177,16 @@ containerItemMid: {
   flex: 1,
   flexBasis: 0,
   flexGrow: 1,
-  borderColor: 'rgba(0, 71, 114, 0.5)',
+  borderColor: 'rgba(0, 71, 114, 0.2)',
   borderRadius: 20,
   flexWrap: 'wrap',
   marginTop: 10,
   marginBottom: 10,
   alignItems: 'center',
+  shadowColor: "#000",
+  shadowOpacity: 1,
+  shadowRadius: 13.16,
+  elevation: 1,
 },
 containerItemBottom: {
   flexDirection: 'row',
@@ -165,12 +196,12 @@ containerItemBottom: {
   marginBottom: 10,
 },
 div1: {
-  backgroundColor: 'rgba(0,0,0,0.2)',
+  backgroundColor: 'rgba(0,0,0,0.4)',
   width: '100%',
   textAlign: 'center',
 },
 div2: {
-  backgroundColor: 'rgba(0,0,0,0.1)',
+  backgroundColor: 'rgba(0,0,0,0.2)',
   width: '100%',
   textAlign: 'center',
 },
@@ -180,6 +211,16 @@ textDisc: {
     textAlign: 'center',
     fontWeight: 'bold',
     color: '#242424'
+},
+textDiscName: {
+  fontSize: 20,
+  color: '#004772',
+  fontWeight: 'bold',
+  //textAlign: 'center'
+},
+textDiscCh: {
+  fontSize: 14,
+  color: '#004772',
 },
 instructions: {
     textAlign: 'center',
@@ -196,11 +237,11 @@ boxTop: {
   borderRadius: 20,
   width: '95%',
   padding: 15,
-  backgroundColor: '#004772',
-  marginTop: 10,
+  marginTop: 5,
   alignItems: 'center',
   borderWidth: 1,
-  borderColor: 'rgba(0,0,0,0.3)' 
+  backgroundColor: '#002f4b',
+  borderColor: 'rgba(0,0,0,0.3)',
 },
 textBoxTop: {
   color: '#ffffff',
@@ -209,10 +250,12 @@ textBoxTop: {
 textCoef: {
   color: '#ffffff',
   fontWeight: 'bold',
-  fontSize: 20,
+  fontSize: 25,
 },
 textTop: {
-  color: '#ffffff'
+  fontSize: 15, 
+  color: '#ffffff',
+  fontWeight: 'bold'
 },
 addButton: {
   borderRadius: 45,
